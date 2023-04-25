bee479b1f527aa53da475db79f6d0d70665dc794
Abolarin Toluwalase <staicky141@gmail.com>
2023-04-25 14:19:16 -0700

    A modification by Tolu and Kufre

--- _printf.c
+++ _printf.c
@@ -30,25 +30,17 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
-			if (format[i + 1] == 'r')
-			{
-				handle_r(buffer, &buf_count);
-				i += 1;
-			}
-			else
-			{
-				print_buffer(buffer, &buf_count);
-				flags = get_flags(format, &i);
-				width = get_width(format, &i, lists);
-				precision = get_precision(format, &i, lists);
-				size = get_size(format, &i);
-				++i;
-				count = handle_print(format, &i, lists, buffer,
-				flags, width, precision, size);
-				if (count == -1)
-					return (-1);
-				char_return += count;
-			}
+			print_buffer(buffer, &buf_count);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, lists);
+			precision = get_precision(format, &i, lists);
+			size = get_size(format, &i);
+			++i;
+			count = handle_print(format, &i, lists, buffer,
+			flags, width, precision, size);
+			if (count == -1)
+				return (-1);
+			char_return += count;
 		}
 	}
 
